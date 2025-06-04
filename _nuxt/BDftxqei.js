13fbe5886d943675b3ad52a9ae8f6bf9f86c5403
Abdalla Dimes <abdalladimes@gmail.com>
2025-06-04 16:28:21 +0300

    feat; Apply small changes

--- _nuxt/BDftxqei.js
+++
@@ -1,1 +0,0 @@
-import{x as s}from"./D_Myowz2.js";const r=s("/icons/close.svg"),t=s("/icons/arrow.svg");export{r as _,t as a};
---
+++ _nuxt/LpQE4U7D.js
@@ -0,0 +1,1 @@
+import{x as s}from"./BFqY9SAo.js";const r=s("/icons/close.svg"),t=s("/icons/arrow.svg");export{r as _,t as a};
